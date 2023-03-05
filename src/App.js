678b276ff6f713de98fe14ccea3f4373906ca72c
Lancet03 <ilja.efremov-2003@mail.ru>
2023-03-05 16:11:39 +0400

    React first components

--- src/App.js
+++ src/App.js
@@ -1,73 +1,14 @@
+import Card from "./components/Card";
+import Header from "./components/Header"
+import Drawer from "./components/Drawer";
+
 function App() {
   return (
     <div className="wrapper">
-
-      <div className="drawerBG">
-        <div className="drawer">
-          <h2>Корзина</h2>
-          <div className="items">
-            <div className="cartItem">
-              <div style={{ backgroundImage: 'url(img/sneakers/1.jpg)' }} className="cartItemImg">
-              </div>
-
-              <div className="cartItemInfo">
-                <p>Мужские Кроссовки Nike Air Max 270</p>
-                <b>12 999 руб.</b>
-              </div>
-              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
-            </div>
-
-            <div className="cartItem">
-              <div style={{ backgroundImage: 'url(img/sneakers/1.jpg)' }} className="cartItemImg">
-              </div>
-
-              <div className="cartItemInfo">
-                <p>Мужские Кроссовки Nike Air Max 270</p>
-                <b>12 999 руб.</b>
-              </div>
-              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
-            </div>
-          </div>
-          <ul>
-            <li>
-              <span></span>
-              <div></div>
-              <b></b>
-            </li>
-            <li>
-              <span></span>
-              <div></div>
-              <b></b>
-            </li>
-          </ul>
-
-        </div>
-      </div>
+     <Drawer/>
+    <Header/>
 
 
-
-
-      <header>
-        <div className="headerLeft">
-          <img src="/img/logo.png"></img>
-          <div className="headerInfo">
-            <h3>React Sneakers</h3>
-            <p>Магазин лучших кроссовок</p>
-          </div>
-        </div>
-        <ul className="headerRight">
-          <li>
-            <img src="/img/cart.svg"></img>
-            <span>1205 руб.</span>
-          </li>
-          <li>
-            <img src="/img/liked.svg"></img>
-          </li>
-          <li>
-            <img src="/img/profile.svg"></img>
-          </li>
-        </ul>
-      </header>
 
 
       <div className="content">
@@ -82,7 +23,7 @@ function App() {
 
 
         <div className="sneakers">
-          <div className="card">
+          {/* <div className="card">
             <div className="favorite">
               <img src="/img/like-off.svg" alt="Unliked" />
             </div>
@@ -97,8 +38,8 @@ function App() {
                 <img width={11} height={11} src="/img/plus.svg" alt="add" />
               </button>
             </div>
-          </div>
-
+          </div> */}
+    <Card />
 
           <div className="card">
             <img width={133} height={112} src="/img/sneakers/2.jpg" alt="sneakers" />
@@ -145,6 +86,7 @@ function App() {
               </button>
             </div>
           </div>
+
         </div>
 
 
