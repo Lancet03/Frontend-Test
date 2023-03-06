5667ab46e605585a94d8cb7432b51f2ba139faad
Lancet03 <ilja.efremov-2003@mail.ru>
2023-03-06 20:19:50 +0400

    props, Css modules

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Card from "./components/Card";
 import Header from "./components/Header"
 import Drawer from "./components/Drawer";
 
+const arr = [
+  {title: "Мужские Кроссовки Nike Blazer Mid Suede", price: 12999, imageUrl: "/img/sneakers/1.jpg"},
+  {title:"Мужские Кроссовки Nike Air Max 270", price: 12999, imageUrl: "/img/sneakers/2.jpg"},
+  {title:"Мужские Кроссовки Nike Blazer Mid Suede", price: 8499, imageUrl: "/img/sneakers/3.jpg"},
+  {title:"Кроссовки Puma X Aka Boku Future Rider", price: 8999, imageUrl: "/img/sneakers/4.jpg"}
+]
+
 function App() {
   return (
     <div className="wrapper">
@@ -23,25 +30,17 @@ function App() {
 
 
         <div className="sneakers">
-          {/* <div className="card">
-            <div className="favorite">
-              <img src="/img/like-off.svg" alt="Unliked" />
-            </div>
-            <img width={133} height={112} src="/img/sneakers/1.jpg" alt="sneakers" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            <div className="cardBottom">
-              <div className="cardPrice">
-                <span>Цена</span>
-                <b>12 999 руб.</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="add" />
-              </button>
-            </div>
-          </div> */}
-    <Card />
+          {
+            arr.map((obj) => (
+              <Card title={obj.title}
+              price={obj.price} 
+              imageUrl = {obj.imageUrl}
+              onClick={() => console.log(obj)}/>
+            ))
+          }
 
-          <div className="card">
+
+          {/* <div className="card">
             <img width={133} height={112} src="/img/sneakers/2.jpg" alt="sneakers" />
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div className="cardBottom">
@@ -85,7 +84,7 @@ function App() {
                 <img width={11} height={11} src="/img/plus.svg" alt="add" />
               </button>
             </div>
-          </div>
+          </div> */}
 
         </div>
 
---
+++ src/components/Card/index.js
@@ -0,0 +1,26 @@
+import styles from "./Card.module.scss"
+
+console.log(styles)
+
+function Card(props){
+    return(
+        <div className={styles.card}>
+        <div className={styles.favorite}>
+          <img src="/img/like-off.svg" alt="Unliked" />
+        </div>
+        <img width={133} height={112} src={props.imageUrl} alt="sneakers" />
+        <h5>{props.title}</h5>
+        <div className="cardBottom">
+          <div className="cardPrice">
+            <span>Цена</span>
+            <b>{props.price}</b>
+          </div>
+          <button className="button" onClick={props.onClick}>
+            <img width={11} height={11} src="/img/plus.svg" alt="add" />
+          </button>
+        </div>
+      </div>
+    );
+}
+
+export default Card;
